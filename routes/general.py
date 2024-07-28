23391d04f8fe1cc4577d7ec90d0acc1b34b5aa1f
Lucas Cerveró Beltrán <lucas@groucho.home>
2024-07-28 14:40:34 +0200

    added new icon, removed unnecessary files and added 404 custom page error

--- routes/general.py
+++ routes/general.py
@@ -14,3 +14,7 @@ def index():
 def about():
     return render_template('about.html')
 
+# Manejador de error 404
+@general_bp.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
